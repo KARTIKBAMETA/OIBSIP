9fe69499dc1e9e5d45896485c0042a98207f2aa9
Kartik Bameta <91360153+KARTIKBAMETA@users.noreply.github.com>
2022-09-11 23:17:02 +0530

    Update game.java

--- game.java
+++ game.java
@@ -3,19 +3,21 @@
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.random.*;
 public class game{
     public static void main(String[] args) {
         Random r=new Random();
         int num=r.nextInt(100)+1;
+        int count=0;
         while(true)
         {
             System.out.println("ENTER A NUMBER BETWEEN 1-100:");
             Scanner sc=new Scanner(System.in);
             int guess=sc.nextInt();
+            count++;
             if(guess==num)
             {
                 System.out.println("Hurrah!! Y O U  W I N...");
+                System.out.println("You took "+count+" tries..");
                 break;
             }
             else if(guess>num)
