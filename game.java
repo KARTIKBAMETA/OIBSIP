4995f6a7aefda5577e4222d7f06eed9b67fbbbbb
Kartik Bameta <91360153+KARTIKBAMETA@users.noreply.github.com>
2022-09-18 22:25:49 +0530

    Update game.java

--- game.java
+++ game.java
@@ -1,8 +1,9 @@
-/*OASIS INFOBYTE JAVA PROGRAMMING TASK-3 */
+/*OASIS INFOBYTE JAVA PROGRAMMING INTERNSHIP TASK-2 */
 //NUMBER GUESSING GAME 
 import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
+import java.util.random.*;
 public class game{
     public static void main(String[] args) {
         Random r=new Random();
@@ -10,24 +11,23 @@ public class game{
         int count=0;
         while(true)
         {
-            System.out.println("ENTER A NUMBER BETWEEN 1-100:");
+            System.out.println("ENTER A NUMBER TO GUESS BETWEEN 1-100");
             Scanner sc=new Scanner(System.in);
             int guess=sc.nextInt();
             count++;
             if(guess==num)
             {
-                System.out.println("Hurrah!! Y O U  W I N...");
-                System.out.println("You took "+count+" tries..");
+                System.out.println("Hurrah!!! Y O U  W I N !");
+                System.out.println("You won in "+count+" tries");
                 break;
             }
             else if(guess>num)
-            {
-                System.out.println("Your guess is too high.. GUESS AGAIN");
-            }
-            else if(guess<num)
-            {
-                System.out.println("Your guess is too low.. GUESS AGAIN");
-            }
+            {System.out.println("Your guess is Too HIGH..Guess Again!!");
+        }
+        else if(guess<num)
+        {
+            System.out.println("Your guess is Too LOW..Guess AGAIN!!");
+        }
         }
     }
 }
